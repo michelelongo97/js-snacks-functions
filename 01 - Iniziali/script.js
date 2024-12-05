ad4f6009b014f02fead23f4fae7d997ea11455d5
michelelongo97 <m.longo7x@gmail.com>
2024-12-05 17:39:22 +0100

    feat: add first solution

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const firstLetter = (names) => {
+    let maiusc = [];
+    for(let i = 0; i < names.length; i++) {
+        maiusc.push(names[i][0]);
+    }
+    return maiusc;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const firstLetterArray = firstLetter(names);
+console.log(firstLetterArray);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
