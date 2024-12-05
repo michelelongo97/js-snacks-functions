f60207a626e2187545c416cd86d52e5a609cbeaa
michelelongo97 <m.longo7x@gmail.com>
2024-12-05 19:26:09 +0100

    geat: add third solution

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+const countVocals = (vocal) => {
+    let vocals = 0;
 
+    for(let i = 0; i < vocal.length; i++) {
+        if (vocal[i] === 'a' || vocal[i] === 'e' || vocal[i] === 'i' || vocal[i] === 'o' || vocal[i] === 'u'){
+
+            vocals = vocals + 1
+        }
+    }
+    return vocals;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const final = countVocals(word)
+console.log(final)
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
