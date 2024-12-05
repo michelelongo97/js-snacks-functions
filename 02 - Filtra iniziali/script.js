d333a18b732d77581891ee3de3dccbc879eaab92
michelelongo97 <m.longo7x@gmail.com>
2024-12-05 19:02:12 +0100

    feat: add second solution

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+const wordsByLetter = (words, letter) => {
+    let final = [];
+    for (let i = 0; i < words.length; i++) {    
+        if (words[i][0] === letter) {
+            final.push(words[i]);
+        }
+    }
+    return final; 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const letter = "A"
+const final = wordsByLetter(names, letter);
 
+console.log(final);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
